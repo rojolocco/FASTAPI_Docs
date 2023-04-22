e9fc9bf8e7296bdc32332a77a15d650b1c34ab69
Christian Garcia <rojolocco@gmail.com>
2023-04-21 22:30:10 -0500

    Query Parameters First Commit

---
+++ query_parameters/main.py
@@ -0,0 +1,26 @@
+# Imports
+from fastapi import FastAPI
+import uvicorn
+
+
+# Routes imports
+from routes import query_route
+
+
+# Application
+app = FastAPI()
+
+
+# Home endpoint
+@app.get("/")
+async def home():
+    return {"message": "Welcome to Home!"}
+
+
+# Query parameters routes
+app.include_router(query_route)
+
+
+# Run main application
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
---
+++ query_parameters/routes.py
@@ -0,0 +1,34 @@
+# Imports
+from fastapi import APIRouter
+from typing import Union
+
+# Route for path parameters
+query_route = APIRouter(prefix="/queryparameters", tags=["Query_Parameters"])
+
+
+# Get Routes
+@query_route.get("/")
+async def get_query(q):
+    return {"message": f"This is a query parameter: {q}"}
+
+
+# Get Routes with one default values
+@query_route.get("/defaultvalue/one")
+async def get_query_with_one_default_value(q: str | None = None):
+    return {"message": f"This is a query parameter: {q}"}
+
+
+# Get Routes with two default values
+@query_route.get("/defaultvalue/two")
+async def get_query_with_two_default_value(
+        q: Union[str, None] = None,
+        p: bool = False):
+    if p:
+        return {"message": f"p is a boolean query parameter: {p}"}
+    return {"message": f"This is a query parameter: {q}"}
+
+
+# Get Routes with one required parameter
+@query_route.get("/requiredvalue/one")
+async def get_query_with_one_required_value(q: str | None):
+    return {"message": f"This is a required query parameter: {q}"}

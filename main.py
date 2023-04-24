053e1c0d33a59d4449d07d9f10a2a6e8a4156c02
Christian Garcia <rojolocco@gmail.com>
2023-04-24 12:42:41 -0500

    Adding routes file

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from fastapi import FastAPI
 import uvicorn
 
 
+# Import routers
+from routes import router
+
+
 # Application
 app = FastAPI()
 
 
 # Home endpoint
-@app.get("/")
+@app.get("/", tags=["Home"])
 async def home():
     return {"message": "Welcome to Home!"}
+
+
+# Include routes
+app.include_router(router)
 
 
 # Run main application
